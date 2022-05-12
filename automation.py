e252d277d1456469ceb9633eed523221e476dafd
polo <lg550055@hotmail.com>
2022-05-12 16:57:59 -0500

    Finds and writes to file emails and phone numbers

--- automation.py
+++ automation.py
@@ -7,4 +7,19 @@ phone_pattern = r'\d{3}-?\)?\d{3}-?\d{4}'
 with open('potential-contacts.txt') as f:
   text = f.read()
 
-print(text[:500])
+# Find all emails, order and write them to emails.txt
+emails = re.findall(email_pattern, text)
+emails.sort()
+with open('emails.txt', 'w') as f:
+  for e in emails:
+    f.write(e+'\n')
+
+# Find all phone numbers, order and write them to phone_numbers.txt
+phones = re.findall(phone_pattern, text)
+phones.sort()
+with open('phone_numbers.txt', 'w') as f:
+  for p in phones:
+    f.write(p+'\n')
+
+print ('Emails found' +len(emails))
+print ('Phones found' +len(phones))
